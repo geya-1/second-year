a3ef95026a9e512f34e32301e504e274835a3673
GDGSVECW <gdg@svecw.edu.in>
2024-10-25 15:56:06 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,8 +1,10 @@
 def reverse_string(s):
     reversed_str = ""
     for char in s:
-        reversed_str = char + reversed_str
-    return reversed_str
+        reversed_str = reversed_str + char 
+    return reversed_string 
 
 # Test the function
-print(reverse_string("Hacktoberfest"))
+print(reverse_string("Hacktoberfest"))  
+print(reverse_string(12345))         
+print(reverse_string(""))             
